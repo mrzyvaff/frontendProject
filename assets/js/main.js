5f005030ce0db9809b48ecf2e4df951df5f65d1b
mrzyvaff <mirzyvaf21@gmail.com>
2024-07-19 14:59:57 +0400

    js

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const learnMoreButton = document.querySelector('.learn-more-button');
+
+learnMoreButton.addEventListener('click', function() {
+    alert('You clicked the Learn More button!');
+});
